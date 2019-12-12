55293376bc3fde5ccb70096a722fa4ff7d1fcef8
sohaYounus97 <sohayounus97@gmail.com>
2019-12-12 16:05:33 +0500

    first commit

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayHealth playHealth;
+    EnemyHealth enemyHealth;
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;
+        playHealth = player.GetComponent <PlayHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+        if(enemyHealth.currentHealth > 0 && playHealth.currentHealth > 0)
+        {
+            nav.SetDestination (player.position);
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Managers/GameOverManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameOverManager : MonoBehaviour
+{
+    public PlayHealth playHealth;
+
+
+    Animator anim;
+
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (playHealth.currentHealth <= 0)
+        {
+            anim.SetTrigger("GameOver");
+        }
+    }
+}
